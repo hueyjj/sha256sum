67f2687e234e168dbd802323bbc272fdd0b3d362
unknown <jasperjeng@gmail.com>
2019-05-16 00:22:47 -0700

    Refactor fractional parts of primes and preprocess section

--- sha256sum.go
+++ sha256sum.go
@@ -5,8 +5,34 @@ import (
 	"fmt"
 )
 
+var (
+	k = []uint32{0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+	}
+)
+
 func rightRotate(value uint32, shift uint) uint32 {
 	return (value >> shift) | (value << (32 - shift))
+}
+
+func preProcess(msg []byte) []byte {
+	// bmsg := []byte(msg)
+	length := uint64(len(msg))
+	K := 448 - 8*(length+1) // K, the number of zeroes for padding to 512-bit
+	padding := make([]byte, K/8)
+
+	size := make([]byte, 8)
+	binary.BigEndian.PutUint64(size, 8*length)
+	msg = append(msg, 0x80)
+	msg = append(msg, padding...)
+	msg = append(msg, size...)
+	return msg
 }
 
 // Sha256Sum generates a SHA-256 hash of a file
@@ -23,27 +49,8 @@ func Sha256Sum(msg string) (string, error) {
 	h6 = 0x1f83d9ab
 	h7 = 0x5be0cd19
 
-	k := []uint32{
-		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-	}
-
 	// Preprocessing
-	bmsg := []byte(msg)
-	length := uint64(len(bmsg))
-	bmsg = append(bmsg, 0x80)
-	K := 448 - len(bmsg)*8 // k, the numer of zeroes for padding to 512-bit
-	padding := make([]byte, K/8)
-	bmsg = append(bmsg, padding...)
-	blength := make([]byte, 8)
-	binary.BigEndian.PutUint64(blength, length*8)
-	bmsg = append(bmsg, blength...)
+	bmsg := preProcess([]byte(msg))
 
 	w := make([]uint32, 64)
 	for i, j := 0, 0; i < 16; i, j = i+1, j+4 {
